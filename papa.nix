10cb1339363ac0f4c33096c1c02ef1c0c69ffb5a
Isaac Elliott <isaace71295@gmail.com>
2017-09-13 17:33:05 +1000

    Removed top-level papa.nix

--- papa.nix
+++
@@ -1,7 +0,0 @@
-with (import <nixpkgs> { }).pkgs;
-import (fetchFromGitHub {
-  repo = "papa";
-  owner = "qfpl";
-  rev = "05bf21fad6176eed1c9bbfbe08f1f058447a510b";
-  sha256 = "0ggv9c789l473k8hxsqcp6mnncw38qkv4v5hjpfln2l479ljafxx";
-})

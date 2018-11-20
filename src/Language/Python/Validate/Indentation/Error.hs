3c38209608dddf9c44494790564c626de248a8a8
Isaac Elliott <isaace71295@gmail.com>
2018-11-20 16:29:10 +1000

    Removed manual TH splicing

--- src/Language/Python/Validate/Indentation/Error.hs
+++ src/Language/Python/Validate/Indentation/Error.hs
@@ -12,9 +12,7 @@ Portability : non-portable
 
 module Language.Python.Validate.Indentation.Error where
 
--- import Control.Lens.TH
-import Control.Lens.Type
-import Control.Lens.Prism
+import Control.Lens.TH
 
 import Language.Python.Syntax.Whitespace
 
@@ -27,54 +25,4 @@ data IndentationError (v :: [*]) a
   | EmptyContinuedLine a
   deriving (Eq, Show)
 
--- makeClassyPrisms ''IndentationError
-class AsIndentationError r_amPW v_aiuW a_aiuX | r_amPW -> v_aiuW
-                                                          a_aiuX where
-  _IndentationError :: Prism' r_amPW (IndentationError v_aiuW a_aiuX)
-  _WrongIndent :: Prism' r_amPW (Indent, Indent, a_aiuX)
-  _TabError :: Prism' r_amPW a_aiuX
-  _ExpectedGreaterThan :: Prism' r_amPW ([Indent], Indents a_aiuX)
-  _ExpectedEqualTo :: Prism' r_amPW ([Indent], Indents a_aiuX)
-  _EmptyContinuedLine :: Prism' r_amPW a_aiuX
-  _WrongIndent = ((.) _IndentationError) _WrongIndent
-  _TabError = ((.) _IndentationError) _TabError
-  _ExpectedGreaterThan = ((.) _IndentationError) _ExpectedGreaterThan
-  _ExpectedEqualTo = ((.) _IndentationError) _ExpectedEqualTo
-  _EmptyContinuedLine = ((.) _IndentationError) _EmptyContinuedLine
-instance AsIndentationError (IndentationError v_aiuW a_aiuX) v_aiuW a_aiuX where
-  _IndentationError = id
-  _WrongIndent
-    = (prism
-          (\ (x1_amQ3, x2_amQ4, x3_amQ5)
-            -> ((WrongIndent x1_amQ3) x2_amQ4) x3_amQ5))
-        (\ x_amQ6
-            -> case x_amQ6 of
-                WrongIndent y1_amQ7 y2_amQ8 y3_amQ9
-                  -> Right (y1_amQ7, y2_amQ8, y3_amQ9)
-                _ -> Left x_amQ6)
-  _TabError
-    = (prism (\ x1_amQb -> TabError x1_amQb))
-        (\ x_amQc
-            -> case x_amQc of
-                TabError y1_amQd -> Right y1_amQd
-                _ -> Left x_amQc)
-  _ExpectedGreaterThan
-    = (prism
-          (\ (x1_amQf, x2_amQg) -> (ExpectedGreaterThan x1_amQf) x2_amQg))
-        (\ x_amQh
-            -> case x_amQh of
-                ExpectedGreaterThan y1_amQi y2_amQj -> Right (y1_amQi, y2_amQj)
-                _ -> Left x_amQh)
-  _ExpectedEqualTo
-    = (prism
-          (\ (x1_amQl, x2_amQm) -> (ExpectedEqualTo x1_amQl) x2_amQm))
-        (\ x_amQn
-            -> case x_amQn of
-                ExpectedEqualTo y1_amQo y2_amQp -> Right (y1_amQo, y2_amQp)
-                _ -> Left x_amQn)
-  _EmptyContinuedLine
-    = (prism (\ x1_amQr -> EmptyContinuedLine x1_amQr))
-        (\ x_amQs
-            -> case x_amQs of
-                EmptyContinuedLine y1_amQt -> Right y1_amQt
-                _ -> Left x_amQs)
+makeClassyPrisms ''IndentationError
--- src/Language/Python/Validate/Scope/Error.hs
+++ src/Language/Python/Validate/Scope/Error.hs
@@ -13,9 +13,7 @@ Portability : non-portable
 
 module Language.Python.Validate.Scope.Error where
 
--- import Control.Lens.TH
-import Control.Lens.Type
-import Control.Lens.Prism
+import Control.Lens.TH
 import Language.Python.Syntax.Ident
 
 data ScopeError (v :: [*]) a
@@ -27,62 +25,4 @@ data ScopeError (v :: [*]) a
   | BadShadowing (Ident v a)
   deriving (Eq, Show)
 
--- makeClassyPrisms ''ScopeError
-class AsScopeError r_ab7T3 v_ab7Nz a_ab7NA | r_ab7T3 -> v_ab7Nz
-                                                        a_ab7NA where
-  _ScopeError ::
-    Control.Lens.Type.Prism' r_ab7T3 (ScopeError v_ab7Nz a_ab7NA)
-  _FoundNonlocal :: Control.Lens.Type.Prism' r_ab7T3 a_ab7NA
-  _FoundGlobal :: Control.Lens.Type.Prism' r_ab7T3 a_ab7NA
-  _DeletedIdent :: Control.Lens.Type.Prism' r_ab7T3 a_ab7NA
-  _FoundDynamic ::
-    Control.Lens.Type.Prism' r_ab7T3 (a_ab7NA, Ident v_ab7Nz a_ab7NA)
-  _NotInScope ::
-    Control.Lens.Type.Prism' r_ab7T3 (Ident v_ab7Nz a_ab7NA)
-  _BadShadowing ::
-    Control.Lens.Type.Prism' r_ab7T3 (Ident v_ab7Nz a_ab7NA)
-  _FoundNonlocal = ((.) _ScopeError) _FoundNonlocal
-  _FoundGlobal = ((.) _ScopeError) _FoundGlobal
-  _DeletedIdent = ((.) _ScopeError) _DeletedIdent
-  _FoundDynamic = ((.) _ScopeError) _FoundDynamic
-  _NotInScope = ((.) _ScopeError) _NotInScope
-  _BadShadowing = ((.) _ScopeError) _BadShadowing
-instance AsScopeError (ScopeError v_ab7Nz a_ab7NA) v_ab7Nz a_ab7NA where
-  _ScopeError = id
-  _FoundNonlocal
-    = (Control.Lens.Prism.prism (\ x1_ab7T4 -> FoundNonlocal x1_ab7T4))
-        (\ x_ab7T5
-            -> case x_ab7T5 of
-                FoundNonlocal y1_ab7T6 -> Right y1_ab7T6
-                _ -> Left x_ab7T5)
-  _FoundGlobal
-    = (Control.Lens.Prism.prism (\ x1_ab7T7 -> FoundGlobal x1_ab7T7))
-        (\ x_ab7T8
-            -> case x_ab7T8 of
-                FoundGlobal y1_ab7T9 -> Right y1_ab7T9
-                _ -> Left x_ab7T8)
-  _DeletedIdent
-    = (Control.Lens.Prism.prism (\ x1_ab7Ta -> DeletedIdent x1_ab7Ta))
-        (\ x_ab7Tb
-            -> case x_ab7Tb of
-                DeletedIdent y1_ab7Tc -> Right y1_ab7Tc
-                _ -> Left x_ab7Tb)
-  _FoundDynamic
-    = (Control.Lens.Prism.prism
-          (\ (x1_ab7Td, x2_ab7Te) -> (FoundDynamic x1_ab7Td) x2_ab7Te))
-        (\ x_ab7Tf
-            -> case x_ab7Tf of
-                FoundDynamic y1_ab7Tg y2_ab7Th -> Right (y1_ab7Tg, y2_ab7Th)
-                _ -> Left x_ab7Tf)
-  _NotInScope
-    = (Control.Lens.Prism.prism (\ x1_ab7Ti -> NotInScope x1_ab7Ti))
-        (\ x_ab7Tj
-            -> case x_ab7Tj of
-                NotInScope y1_ab7Tk -> Right y1_ab7Tk
-                _ -> Left x_ab7Tj)
-  _BadShadowing
-    = (Control.Lens.Prism.prism (\ x1_ab7Tl -> BadShadowing x1_ab7Tl))
-        (\ x_ab7Tm
-            -> case x_ab7Tm of
-                BadShadowing y1_ab7Tn -> Right y1_ab7Tn
-                _ -> Left x_ab7Tm)
+makeClassyPrisms ''ScopeError

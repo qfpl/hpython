0f747e4d4e12d18355f67baee2fc496ef30cc6d4
Isaac Elliott <isaace71295@gmail.com>
2019-01-07 12:56:28 +1000

    Docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nix-shell --run "cabal new-haddock --ghc-options=-fforce-recomp && cp -R dist-newstyle/build/x86_64-linux/ghc-8.4.3/hpython-0.1.0.0/doc/html/hpython/* docs"
+nix-shell --run "cabal new-haddock --ghc-options=-fforce-recomp && cp -R dist-newstyle/build/x86_64-linux/ghc-8.4.4/hpython-0.1/doc/html/hpython/* docs"
